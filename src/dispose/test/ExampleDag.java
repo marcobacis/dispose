e594deb7dee1d934c6564e5779eceac7e9b94487
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-08-09 17:20:08 +0200

    Move ClientName to test

--- src/dispose/test/ExampleDag.java
+++ src/dispose/test/ExampleDag.java
@@ -1,7 +1,13 @@
 
-package dispose.client;
+package dispose.test;
 
-public class ClientMain
+import dispose.client.ClientDag;
+import dispose.client.FileConsumerStream;
+import dispose.client.FileProducerStream;
+import dispose.client.Op;
+import dispose.client.Stream;
+
+public class ExampleDag
 {
   public static void main(String[] args)
   {
