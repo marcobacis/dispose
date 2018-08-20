4e1ab501bf794ba7b999ffd4146a9721567ddcfd
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-08-20 17:13:19 +0200

    Remove redundant code

--- src/dispose/net/message/chkp/ChkpRequestMsg.java
+++ src/dispose/net/message/chkp/ChkpRequestMsg.java
@@ -1,39 +1,29 @@
 package dispose.net.message.chkp;
 
-import java.util.UUID;
-
 import dispose.net.message.CtrlMessage;
 import dispose.net.node.Node;
 
 public class ChkpRequestMsg extends CtrlMessage
 {
-
   private static final long serialVersionUID = -6339978017034829113L;
-  private UUID uuid = UUID.randomUUID();
-  
   private int chkpId;
+  
   
   public ChkpRequestMsg(int id)
   {
     this.chkpId = id;
   }
   
+  
   public int getID()
   {
     return chkpId;
   }
   
-  @Override
-  public UUID getUUID()
-  {
-    return uuid;
-  }
   
   @Override
   public void executeOnNode(Node node)
   {
     node.injectIntoSource(this);
   }
-  
-
 }
