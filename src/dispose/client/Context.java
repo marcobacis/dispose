756d3e29f2c924319cec5e1e0fe5f6e450085184
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-08-22 16:18:07 +0200

    Add more thread names

--- src/dispose/client/Context.java
+++ src/dispose/client/Context.java
@@ -1,3 +1,4 @@
+
 package dispose.client;
 
 import java.io.IOException;
@@ -20,23 +21,25 @@ import dispose.net.supervisor.NodeProxy;
 import dispose.net.supervisor.Supervisor;
 import dispose.test.ExampleJob;
 
+
 public class Context
 {
   private boolean remoteSup = false;
   private String remoteSupervisorAddress = "";
-  
+
   private boolean ready = false;
-  
+
   private Node localNode;
   private Supervisor localSupervisor;
   private Thread localSupervisorThread;
   private Thread localNodeThread;
   private Link ctrlLink;
-  
+
+
   public Context()
   {
     remoteSup = false;
-    
+
     try {
       initializeLocal();
       ready = true;
@@ -44,87 +47,93 @@ public class Context
       ready = false;
       DisposeLog.critical(this, "Error during context initialization: ", e);
     }
-    
+
   }
-  
+
+
   public Context(String supervisorAddress)
   {
     try {
       remoteSup = true;
       remoteSupervisorAddress = supervisorAddress;
-      
+
       initializeRemote();
-      
+
       ready = true;
-      
+
     } catch (IOException e) {
       ready = false;
       DisposeLog.critical(this, "Error during context initialization: ", e);
     }
   }
-  
+
+
   private void initializeRemote() throws UnknownHostException, IOException
   {
     remoteSup = true;
-    
+
     System.out.println(remoteSupervisorAddress);
     ctrlLink = SocketLink.connectTo(remoteSupervisorAddress, Config.nodeCtrlPort);
     localNode = new Node(ctrlLink);
     localNodeThread = new Thread(localNode);
     localNodeThread.start();
   }
-  
+
+
   private void initializeLocal() throws LinkBrokenException
   {
     remoteSup = false;
-    
+
     localSupervisor = new Supervisor();
     localSupervisorThread = new Thread(localSupervisor);
+    localSupervisorThread.setName("local-supervisor");
     localSupervisorThread.start();
     ObjectFifoLink _localLinkA = new ObjectFifoLink();
     ObjectFifoLink _localLinkB = new ObjectFifoLink();
     _localLinkA.connect(_localLinkB);
-    
+
     ctrlLink = _localLinkA;
-    
+
     localNode = new Node(ctrlLink);
     localNodeThread = new Thread(localNode);
+    localNodeThread.setName("local-node");
     localNodeThread.start();
     localSupervisor.registerNode(new NodeProxy(localSupervisor, _localLinkB, NodeProxy.LOCAL_NETWORK_ADDRESS));
   }
-  
+
+
   public void run(Stream consumer) throws LinkBrokenException, NotAcknowledgeableException, InterruptedException
   {
-    if(!ready) {
-      DisposeLog.critical(this, "The job is not reay due to errors. Restart the application to try again.");
+    if (!ready) {
+      DisposeLog.critical(this, "The job is not ready due to errors. Restart the application to try again.");
       return;
     }
-    
+
     ClientDag compDag = ClientDag.derive(consumer);
-    
+
     UUID jobid = UUID.randomUUID();
     CtrlMessage newmsg = new CreateJobMsg(jobid, compDag);
     localNode.getControlLink().sendMsgAndRequestAck(newmsg);
     localNode.getControlLink().waitAck(newmsg);
-    
+
     CtrlMessage idmsg = new JobCommandMsg(jobid, Command.START);
     localNode.getControlLink().sendMsgAndRequestAck(idmsg);
     localNode.getControlLink().waitAck(idmsg);
-    
+
     DisposeLog.info(ExampleJob.class, "the dag has been instantiated!");
     DisposeLog.debug(ExampleJob.class, compDag);
-    
+
     localNode.waitJobCompleted(jobid);
-    
+
     // TODO if not remote, wait for the local supervisor thread to die
     // other TODO : check that the local node thread joins!
-    if(!remoteSup) {
-      //something to stop the supervisor goes here
+    if (!remoteSup) {
+      // something to stop the supervisor goes here
       localSupervisorThread.join();
     }
     localNodeThread.join();
-    
+
     DisposeLog.info(this, "The End");
   }
-  
+
 }
